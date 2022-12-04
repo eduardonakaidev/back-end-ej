aff634b978261f1b14a19f6037731b3d42f12ecd
eduardo issao nakai frasson <eduardofrnkdev@gmail.com>
2022-12-03 22:52:32 -0300

    a

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const express = require('express');
 const Produtos = require('./Produtos');
 const app = express();
 
+app.use(express.json())
+
 app.get('/',async(req,res)=>{
     res.send('Pagina iniciada ')
 })
 
 app.post('/cadastrar',async(req,res)=>{
+    await Produtos.create(req.body).then(()=>{
+        .then(()=>{
+            return res.json({
+                erro:false,
+                mensagem:'usuario cadastrado com sucesso '
+            })
+
+        })
+        return res.status(400).json({
+            erro:false,
+            mensagem:'Erro:usuario nao cadastrado '
+        })
+    })
     res.send('Pagina cadastrar')
 })
 
